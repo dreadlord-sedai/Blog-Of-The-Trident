0cc5ff5a4cb5dfa6a6fbb807d7f9c109fd63d532
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-24 16:33:05 +0530

    Import Flask-WTF configurations to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes # Import routes after creating `app` to avoid circular import issues.
