18291ee074842a1d3b51bed0c33c9fa471811c16
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-24 15:37:53 +0530

    Render index in rouutes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app  # Import the `app` instance from `__init__.py`
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Fabio'}
+    return render_template('index.html', title='Home', user=user)
