23ee2e0dbd2becb186c5d1d05b91e9eafaee7d97
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-24 15:58:45 +0530

    Create base.html

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from flask import render_template
 from app import app  # Import the `app` instance from `__init__.py`
 
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Fabio'}
-    return render_template('index.html', title='Home', user=user)
+    posts=[
+        {
+            'author': {'username': 'Robb Stark'},
+            'body': 'I am the Young Wolf.'
+        },
+        {
+            'author': {'username': 'Greatjon Umber'},
+            'body': 'The King in the North!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
