68b605687fd476c73d175bb7b50d33b7d2c3f642
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-25 10:07:44 +0530

    Code flask-wtf form validation for login and show the flash messege in index

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app  # Import the `app` instance from `__init__.py`
 from app.forms import LoginForm
 
@@ -24,4 +24,9 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
