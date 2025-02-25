a5d01ab19df46dd3caae1019c50bf98fa3d67600
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-25 09:50:41 +0530

    Code loginn route logic

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,7 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template('login.html', title='Sign In', form=form)
