fe27e7e6696ac4fc1492d0a0c2f1a507f8427384
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-26 09:03:32 +0530

    Write Login view function Logic

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,45 @@
 from flask import render_template, flash, redirect, url_for
 from app import app  # Import the `app` instance from `__init__.py`
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 
-
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username': 'Fabio'}
-    posts=[
-        {
-            'author': {'username': 'Robb Stark'},
-            'body': 'I am the Young Wolf.'
-        },
-        {
-            'author': {'username': 'Greatjon Umber'},
-            'body': 'The King in the North!'
-        }
+    user = {"username": "Fabio"}
+    posts = [
+        {"author": {"username": "Robb Stark"}, "body": "I am the Young Wolf."},
+        {"author": {"username": "Greatjon Umber"}, "body": "The King in the North!"},
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
+
+    """ 
+    This code checks if the form has been submitted and is valid. If so,
+    it queries the database for a user with a username matching the one entered in the form.
+    The result is stored in the user variable. 
+    """
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
-        ))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+
+        user = db.session.scalar(  # The db.session.scalar method is used to execute a scalar query, which returns a single value
+            sa.select(User).where(User.username == form.username.data)
+        )
+
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("index"))
+
+    return render_template("login.html", title="Sign In", form=form)
