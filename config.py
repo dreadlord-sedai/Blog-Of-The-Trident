38461f0d58a10185245c7b791c4e82e2b9dc420a
dreadlord-sedai <dahamifabbio@gmail.com>
2025-02-27 09:45:00 +0530

    Add email server details

--- config.py
+++ config.py
@@ -2,8 +2,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    # The SECRET_KEY configuration variable is an important part in most Flask applications.
+    # It is used by Flask and many of its extensions to encrypt data, including cookies, request data, and the CSRF token.
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     
+    # This configuration option is used to signal the application every time a change is about to be made in the database.
+    # Email Server Details
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') # or 'smtp.googlemail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25) 
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None 
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['dahamifabbio@gmail.com']
     
