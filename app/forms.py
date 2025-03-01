3a668c3b9c447ee6c48eb18e3cd9e450b4613995
dreadlord-sedai <dahamifabbio@gmail.com>
2025-03-01 08:36:39 +0530

    Create blog post submisission form

--- app/forms.py
+++ app/forms.py
@@ -59,3 +59,9 @@ class EditProfileForm(FlaskForm):
 # This is used for forms that do not have any fields, such as the follow and unfollow forms.
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
