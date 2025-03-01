67cce091f664c49262c94cf6cef576618c68cc29
dreadlord-sedai <dahamifabbio@gmail.com>
2025-03-01 08:43:59 +0530

    Code post submission form in index route

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,21 @@ from datetime import datetime, timezone
 import sqlalchemy as sa
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db # Import the `app` instance from `__init__.py`
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/" , methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'Robb Stark'},
@@ -22,7 +29,7 @@ def index():
             'body': 'The King in the North!'
         }
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home Page", form=form, posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
