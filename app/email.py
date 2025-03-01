10e599da1839f480e43576242f47f3422301b11c
dreadlord-sedai <dahamifabbio@gmail.com>
2025-03-01 11:30:43 +0530

    Create email sending wrapper function

---
+++ app/email.py
@@ -0,0 +1,10 @@
+from flask_mail import Message
+from app import mail
+
+# wrapper function to send email
+# Wraps the Message class from Flask-Mail to send an email
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
