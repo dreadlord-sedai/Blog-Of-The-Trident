476c47addd581dd0a8fadb3ce04fadf8a5ac0525
dreadlord-sedai <dahamifabbio@gmail.com>
2025-03-02 11:32:49 +0530

    Add command line enhancements

--- app/cli.py
+++ app/cli.py
@@ -1,12 +1,14 @@
 from app import app
 import os
+import click
 
 @app.cli.group()
 def translate():
     """Translation and localization commands."""
     pass
 
-
+# The update command extracts all the text marked for translation in the application and updates 
+# the language catalogs with the new texts.
 @translate.command()
 def update():
     """Update all languages."""
@@ -21,3 +23,16 @@ def compile():
     """Compile all languages."""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
+    
+
+# The init command creates a new language catalog based on the messages.pot file.
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system(
+            'pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
