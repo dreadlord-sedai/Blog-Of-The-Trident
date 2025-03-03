3997602591887c5aac3929761e9d04533c7290b4
dreadlord-sedai <dahamifabbio@gmail.com>
2025-03-03 16:03:25 +0530

    Restructure app cli.py

--- app/cli.py
+++ app/cli.py
@@ -1,31 +1,16 @@
-from app import app
 import os
+from flask import Blueprint
 import click
 
-@app.cli.group()
+bp = Blueprint('cli', __name__, cli_group=None)
+
+
+@bp.cli.group()
 def translate():
     """Translation and localization commands."""
     pass
 
-# The update command extracts all the text marked for translation in the application and updates 
-# the language catalogs with the new texts.
-@translate.command()
-def update():
-    """Update all languages."""
-    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-        raise RuntimeError('extract command failed')
-    if os.system('pybabel update -i messages.pot -d app/translations'):
-        raise RuntimeError('update command failed')
-    os.remove('messages.pot')
 
-@translate.command()
-def compile():
-    """Compile all languages."""
-    if os.system('pybabel compile -d app/translations'):
-        raise RuntimeError('compile command failed')
-    
-
-# The init command creates a new language catalog based on the messages.pot file.
 @translate.command()
 @click.argument('lang')
 def init(lang):
@@ -36,3 +21,20 @@ def init(lang):
             'pybabel init -i messages.pot -d app/translations -l ' + lang):
         raise RuntimeError('init command failed')
     os.remove('messages.pot')
+
+
+@translate.command()
+def update():
+    """Update all languages."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel update -i messages.pot -d app/translations'):
+        raise RuntimeError('update command failed')
+    os.remove('messages.pot')
+
+
+@translate.command()
+def compile():
+    """Compile all languages."""
+    if os.system('pybabel compile -d app/translations'):
+        raise RuntimeError('compile command failed')
